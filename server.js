17d9e5628b935608a9e279b0cbc1a732ae7f7366
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-04-11 12:48:54 -0700

    Changed file paths in server.js and added engines and node in server.js to trouble shoot heroku deployment

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const port = 8080;
-const mainDir = path.join(__dirname, "./Develop/public");
+//const port = 8080;
+const port = process.env.PORT || 8080;
+//const mainDir = path.join(__dirname, "./Develop/public");
+const mainDir = path.join(__dirname, "Develop/public");
 
 // Middleware for serving static files
-app.use(express.static('./Develop/public'));
+//app.use(express.static('./Develop/public'));
+app.use(express.static('Develop/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -19,23 +22,26 @@ app.use(express.json());
 //__dirname = Node.js global variable= represents the current directory of the server.js file, and the "notes.html" file path is constructed by joining the __dirname with the relative path to the file
 // This way can ensure that the file path is correct regardless of where server.js file is located in the file system.
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+    //res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 
 // Route for handling GET request to "/api/notes" URL
 app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./Develop/db/db.json"));
+    //res.sendFile(path.join(__dirname, "./Develop/db/db.json"));
+    res.sendFile(path.join(__dirname, "Develop/db/db.json"));
 });
 
 // Route for handling GET request to "/api/notes/:id" URL
 app.get("/api/notes/:id", (req, res) => {
-    const filteredNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
+    //const filteredNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
+    const filteredNotes = JSON.parse(fs.readFileSync("Develop/db/db.json", "utf8"));
     res.json(filteredNotes[Number(req.params.id)]);
 });
 
 // Route for handling POST request to "/api/notes" URL
 app.post("/api/notes", (req, res) => {
-    const filteredNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
+    //const filteredNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
+    const filteredNotes = JSON.parse(fs.readFileSync("Develop/db/db.json", "utf8"));
     const newNote = req.body;
     const uniqueID = (filteredNotes.length).toString();
     newNote.id = uniqueID;
@@ -47,7 +53,8 @@ app.post("/api/notes", (req, res) => {
 
 // Route for handling DELETE request to "/api/notes/:id" URL
 app.delete("/api/notes/:id", (req, res) => {
-    const savedNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
+    //const savedNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
+    const savedNotes = JSON.parse(fs.readFileSync("Develop/db/db.json", "utf8"));
     const noteID = req.params.id;
     let newID = 0;
     console.log(`Deleting note with ID ${noteID}`);
@@ -61,7 +68,8 @@ app.delete("/api/notes/:id", (req, res) => {
     }
     //fs.writeFileSync() method, passing in the path to the file and the data you want to write as arguments
     //fs.writeFileSync('path to file', JSON.stringify(data));
-    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(filteredNotes));
+    //fs.writeFileSync("./Develop/db/db.json", JSON.stringify(filteredNotes));
+    fs.writeFileSync("Develop/db/db.json", JSON.stringify(filteredNotes));
     res.json(filteredNotes);
 });
 
